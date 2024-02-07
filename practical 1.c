f0e46bf625c17c8c5d8082af0edefd1d8f495415
YashChoksi <159264784+YashChoksi2645@users.noreply.github.com>
2024-02-07 18:57:40 +0530

    Update practical 1.c

--- practical 1.c
+++ practical 1.c
@@ -1,141 +1,154 @@
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include<string>
+#include<iomanip>
 using namespace std;
 
-void print_marksheet();
-string convert(int);
-int sgpa(int);
 int main()
 {
-    print_marksheet();
-    return 0;
+    char id[7];
+    string stname,subname[3],tgrade[3],pgrade[3];
+    int sem,i,thmarks[3],pmarks[3];
+    int tgradept[3],pgradept[3];
+    cout << "Enter Student ID : " ;
+    cin>>id;
+    fflush(stdin);
+    cout << "Enter Student Name : ";
+    cin>>stname;
+    fflush(stdin);
+    cout << "Enter the Semester : ";
+    cin>>sem;
+    fflush(stdin);
+
+    for(i=0;i<3;i++)
+    {
+        cout<<"enter subject name :";
+        getline(cin,subname[i]);
+        fflush(stdin);
+        cout<<"enter theory marks of "<<subname[i]<<" :";
+        cin>>thmarks[i];
+        fflush(stdin);
+        cout<<"enter practical marks of "<<subname[i]<<" :";
+        cin>>pmarks[i];
+        fflush(stdin);
+    }
+
+    for(i=0;i<3;i++)
+    {
+        if(thmarks[i]>=80)
+       {
+            tgrade[i]="AA";
+            tgradept[i]=10;
+       }
+        else if(thmarks[i]<80 && thmarks[i]>=73)
+        {
+            tgrade[i]="AB";
+            tgradept[i]=9;
+        }
+        else if(thmarks[i]<73 && thmarks[i]>=66)
+        {
+            tgrade[i]="BB";
+            tgradept[i]=8;
+        }
+        else if(thmarks[i]<66 && thmarks[i]>=60)
+        {
+            tgrade[i]="BC";
+            tgradept[i]=7;
+        }
+        else if(thmarks[i]<60 && thmarks[i]>=55)
+        {
+            tgrade[i]="CC";
+            tgradept[i]=6;
+        }
+        else if(thmarks[i]<55  && thmarks[i]>=50)
+        {
+            tgrade[i]="CD";
+            tgradept[i]=5;
+        }
+        else if(thmarks[i]<50  && thmarks[i]>=45)
+        {
+            tgrade[i]="DD";
+            tgradept[i]=4;
+        }
+        else if(thmarks[i]<45)
+        {
+            tgrade[i]="FF";
+            tgradept[i]=0;
+        }
+
+        if(pmarks[i]>=80)
+        {
+            pgrade[i]="AA";
+            pgradept[i]=10;
+        }
+        else if(pmarks[i]<80 && pmarks[i]>=73)
+        {
+            pgrade[i]="AB";
+            pgradept[i]=9;
+        }
+        else if(pmarks[i]<73 && pmarks[i]>=66)
+        {
+            pgrade[i]="BB";
+            pgradept[i]=8;
+        }
+        else if(pmarks[i]<66 && pmarks[i]>=60)
+        {
+            pgrade[i]="BC";
+            pgradept[i]=7;
+        }
+        else if(pmarks[i]<60 && pmarks[i]>=55)
+        {
+            pgrade[i]="CC";
+            pgradept[i]=6;
+        }
+        else if(pmarks[i]<55 && pmarks[i]>=50)
+        {
+            pgrade[i]="CD";
+            pgradept[i]=5;
+        }
+        else if(pmarks[i]<50 && pmarks[i]>=45)
+        {
+            pgrade[i]="DD";
+            pgradept[i]=4;
+        }
+        else if(pmarks[i]<45)
+        {
+            pgrade[i]="FF";
+            pgradept[i]=0;
+        }
+    }
+
+    int cigi=0,ci=18;
+    float sgpa;
+    for(i=0;i<3;i++)
+    {
+        cigi=cigi+(pgradept[i]*3)+(tgradept[i]*3);
+    }
+
+    sgpa=(float)cigi/ci;
+
+    system("cls");
+    cout<<endl<<"Student ID    :"<<id<<endl;
+    cout<<"Student name  :"<<stname<<endl;
+    cout<<"Semester      :"<<sem<<endl;
+
+
+    cout<<"\t\t\t\tTheory\tPractical"<<endl;
+    for(i=0;i<3;i++)
+    {
+        cout<<left<<setw(30)<<subname[i]<<"\t"<<tgrade[i]<<"\t"<<pgrade[i]<<endl;
+    }
+
+
+    cout<<endl<<"SGPA          :"<<setprecision(2)<<sgpa;
+
+
 }
 
-void print_marksheet()
-{
-    char stu_id[8], name_of_stu[20], subject[3][30];
-    int semester,sum1=0,sum2=0,credit_sum=0;
-    float Sgpa;
-    string th_grade[3], pa_grade[3];
-    int th_mark[3], pa_mark[3];
-    int th_g[3],pa_g[3],th_c[]={3,3,3},pa_c[]={1,1,1};
 
-    cout << "Enter student id:";
-    cin >> stu_id;
-    cout << "Enter Name of student:";
-    cin >> name_of_stu;
-    cout << "Enter the Semester:";
-    cin >> semester;
+   
+   
 
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "Enter " << i + 1 << " subject name:";
-        cin >> subject[i];
-        cout << "Enter marks for " << subject[i] << " (Theory Practical): "<<endl;
-        cin >> th_mark[i] >> pa_mark[i];
-        th_grade[i] = convert(th_mark[i]);
-        pa_grade[i] = convert(pa_mark[i]);
-        th_g[i]=sgpa(th_mark[i]);
-        pa_g[i]=sgpa(pa_mark[i]);
-        sum1+=th_g[i]*th_c[i];
-        sum2+=pa_g[i]*pa_c[i];
-        credit_sum+=th_c[i]+pa_c[i];
-    }
-    Sgpa=(float)(sum1+sum2)/credit_sum;
-
-    cout.width(15);
-    cout << left << "Student ID"
-         << ":" << stu_id << endl;
-    cout.width(15);
-    cout << left << "Student Name"
-         << ":" << name_of_stu << endl;
-    cout.width(15);
-    cout << left << "Semester"
-         << ":" << semester << endl;
-    cout << endl;
-    cout.width(25);
-    cout << left << "Subject";
-    cout << setw(10) << "Theory"
-         << "Practical" << endl;
-
-    for (int i = 0; i < 3; i++)
-    {
-        cout << left << setw(25) << subject[i];
-        cout << setw(10) << th_grade[i] << setw(10) << pa_grade[i];
-        cout << endl;
-    }
-    cout<<endl<<"SGPA:"<<setprecision(3)<<Sgpa;
-}
-
-string convert(int mark)
-{
-    if (mark >= 80)
-    {
-        return "AA";
-    }
-    else if (mark >= 73 && mark < 80)
-    {
-        return "AB";
-    }
-    else if (mark >= 66 && mark < 73)
-    {
-        return "BB";
-    }
-    else if (mark >= 60 && mark < 66)
-    {
-        return "BC";
-    }
-    else if (mark >= 55 && mark < 60)+
-    {
-        return "CC";
-    }
-    else if (mark >= 50 && mark < 55)
-    {
-        return "CD";
-    }
-    else if (mark >= 45 && mark < 50)
-    {
-        return "DD";
-    }
-    else if (mark < 45)
-    {
-        return "FF";
-    }
-   return 0; // Return an empty string if none of the conditions are met
-}
-
-int sgpa(int mark){
-    if (mark >= 80)
-    {
-        return 10;
-    }
-    else if (mark >= 73 && mark < 80)
-    {
-        return 9;
-    }
-    else if (mark >= 66 && mark < 73)
-    {
-        return 8;
-    }
-    else if (mark >= 60 && mark < 66)
-    {
-        return 7;
-    }
-    else if (mark >= 55 && mark < 60)
-    {
-        return 6;
-    }
-    else if (mark >= 50 && mark < 55)
-    {
-        return 5;
-    }
-    else if (mark >= 45 && mark < 50)
-    {
-        return 4;
-    }
-    else if (mark < 45)
-    {
-        return 0;
-    }
-    return 0;
-}
+    
+   
+    
+   
